e7f6729fa484e46936fbe2a40a36f93fc77ee2b4
JannisKoeksel <65769490+JannisKoeksel@users.noreply.github.com>
2021-04-18 18:20:59 +0200

    Gas optimisation, reduced flexebility

--- BEP20-crowdsale.sol
+++ BEP20-crowdsale.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.3;
-// suplies owner functionality ( standart remix exaple)
+// suplies owner functionality (standart remix example)
 import "./contracts/2_Owner.sol";
 // supplies Safe Math and standart BEP20 Token 
 import "./BEP20Token.sol";
@@ -24,8 +24,10 @@ import "./BEP20Token.sol";
 
 
 
-contract CrowdSale is Owner {
+contract CrowdSaleAdvanced is Owner {
     using SafeMath for uint256;
+    
+    string public name;
     
     // The amount of token a address has bought but not withdrawn
     mapping (address => uint256) private _balances;
@@ -33,47 +35,39 @@ contract CrowdSale is Owner {
      // The amount of token a address has withdrawn and not given back
     mapping (address => uint256) private _withdrawn;
     
-    // address of the token being sold 
+    
+    
+   
     address private _tokenContract;
-    // address of address where BNB will be send to after sale is over 
     address payable private _vault;
     
     // the token for sale ( Interface has to be known while compiling)
     BEP20Token private  Token;
     
     
-    // the minimum amount of BNB per sale (keep in mind that BNB 18 decimals)
-    uint256 public minimumBuy;
-    // the maximum amount of BNB per sale (keep in mind that BNB 18 decimals)
+   
+    uint256 public minimumBuy; // in BNB 
     uint256 public maximumBuy;
-    
     // the contract does not track if one address buys token multiple times 
     
-    // amount of token per wBNB ( analog to wei)
-    uint256 public saleMultiplier;
+    uint256 public saleMultiplier; // tokens per BNB
     
     // total amount of tokens for sale ( has to be smaller thant the token balance of the contrakt for the sale to be activated)
-    uint256 public tokensForSale;
+    uint256 private tokensForSale;
     
-    // the amount of token already sold
+
     uint256 private tokensSold;
-    
-    // the amount of tokens already withrawn ( decreases the contract token balance)
     uint256 private tokensWithdrawn;
-    
-    // determains if token can be bought (initial value is false)
-    bool public saleActive;
+    uint256 public refundPercentage; 
     
     // unix timestamp : time when tokens can be withdrawn 
     uint256 public tokenLock;
-    
     // unix timestamp : time when BNB can be withdrawn to _vault
     uint256 public fundLock;
     
-    // the refundable share of BNB in percent 
-    uint256 public refundPercentage;
+
     
-    // amin events 
+
     event Buy(address buyer , uint256 value , uint256 amount);
     event Withdraw(address buyer, uint amount);
     
@@ -81,44 +75,26 @@ contract CrowdSale is Owner {
     
     constructor() {
         // example values 
-        minimumBuy = 1000000000000000000 ;
+        minimumBuy = 1 ;
         maximumBuy = 10000000000000000000;
         saleMultiplier = 9000;
         tokensForSale = 10000000000000000000000000;
         refundPercentage = 80;
-    }
-    
-    
-    // set address where BNB funds will be send to 
-    function setVault(address payable vault) external isOwner {
-        _vault = vault;
-    }
-    
-    // set token contract address
-    function setContract(address tokenContractAddress) external isOwner {
-        // sale cant be active when token contract changes (token should not be chnaged , rather deploy a new contract)
-        require(!saleActive, "Cant change while sale is active");
+        name = "name";
+        _vault = payable(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+        _tokenContract = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;
         
-        _tokenContract = tokenContractAddress;
         Token = BEP20Token(_tokenContract);
     }
     
-     // get token contract adress 
+    
+
+
     function tokenContract() external view returns(address){
         return _tokenContract;
     }
     
-    
-    // toggle sale state ( active / inactive) initial state is inactive ^= false
-    // requires a valid token contract address and the token in contract >= tokens for sale
-    function toggleSale() external isOwner {
-        require(_tokenContract != address(0), "Token contract address is zero." );
-        require( tokensForSale <= Token.balanceOf(address(this)) , "The contracts tokens balance is too small for this transaction. The admin has to increase the tokan balance." );
-        saleActive = !saleActive;
-    }
-    
-    // get amount of locked token for address
-    // returns the addreses token value 
+
     function balanceOf(address account) external view returns (uint256) {
         return _balances[account];
     }
@@ -126,41 +102,32 @@ contract CrowdSale is Owner {
     // -----------------------------------------
     // Crowdsale main function external 
     // -----------------------------------------
+    
     function buy(address buyer) external payable {
         _buy(buyer,msg.value);
         
     }
     
     
-    /// internal buy logic 
     function _buy(address buyer, uint256 value) internal {
-        // calculate amount of token based on amount = BNB * saleMultiplier
-        uint256 amount = value.mul(saleMultiplier);
-         
-        require(saleActive, "There is no active token sale");
         
-        // require Value to be in set sale bouns by ( minimum/maximum Buy)
+        uint256 amount = value.mul(saleMultiplier);
+        uint256 tokensSold_local = tokensSold.add(amount);
+         
         require(msg.value >= minimumBuy, "BNB mount to small");
         require(msg.value <= maximumBuy, "BNB amount to large");
+        require(tokensSold_local <= tokensForSale);
          
-         // set balance of buyer to amount 
          _balances[buyer] = _balances[buyer].add(amount);
+         tokensSold = tokensSold_local;
          
-         // implements requre the available amount of tokens to be bigger than the purchsaed amount 
-         // decrese tokens for sale amount 
-         tokensForSale = tokensForSale.sub(amount);
-         
-         //increase tokens sold 
-         tokensSold = tokensSold.add(amount);
-         
-        
          emit Buy(buyer,value, amount);
          
     }
 
     
     
-    // withdraw bought tokens after timelock opens 
+  
     function withdrawToken() external {
         
          // define local balace to prevent Re-Entrancy
@@ -176,7 +143,7 @@ contract CrowdSale is Owner {
         require(local_balance > 0 , "The balance for this account is zero");
         
         // transfer tokens to address
-        Token.transfer(msg.sender,local_balance );
+        require(Token.transfer(msg.sender,local_balance ));
         
         // add withdrawn tokens amount to total withdrwan amount
         tokensWithdrawn = tokensWithdrawn.add( local_balance);
@@ -190,18 +157,6 @@ contract CrowdSale is Owner {
 
     } 
     
-    // withdraw BNB after fund timelock opens 
-    function withdrawFunds(uint256 gas) external view isOwner {
-        
-        // require vault to be valid and timelock to be open 
-         require(_vault != address(0) , "The vault address is zero");
-         require(fundLock < block.timestamp, "Timelock is active.");
-         
-         // send BNB to _vault ( gas variable to ensure funds are retreavable )
-         _vault.call{gas: gas, value: address(this).balance};
-    }
-    
-    // ############## remove if token refund not possible ###############
     // refund bought tokens , requires allowance for for contract to equalt the returnd amount 
     function refund(uint256 amount, address payable receiver) external {
         
@@ -223,18 +178,31 @@ contract CrowdSale is Owner {
         
     }
     
+        // withdraw BNB after fund timelock opens 
+    function withdrawFunds(uint256 gas) external view isOwner {
+        
+        // require vault to be valid and timelock to be open 
+         //require(_vault != address(0) , "The vault address is zero");
+         require(fundLock < block.timestamp, "Timelock is active.");
+         
+         // send BNB to _vault ( gas variable to ensure funds are retreavable )
+         _vault.call{gas: gas, value: address(this).balance};
+    }
+    
     
     // destruct contract 
     function destruct() external isOwner {
         // require vault to be valid AND funds timelock to be open AND all tokens to be withdrawn 
         
-        require(_vault != address(0) , "The vault address is zero");
+        //require(_vault != address(0) , "The vault address is zero");##############
         require(fundLock < block.timestamp, "Timelock is active.");
         require(tokensWithdrawn == tokensSold , "Token withdraw not done");
         
+        //require(_tokenContract != address(0) ); #######################
+        
         // If not all tokens have been sold transfer them back to owner 
         if( Token.balanceOf(address(this)) > 0 ) {
-            Token.transfer( _owner(), Token.balanceOf(address(this)) );
+            require(Token.transfer( _owner(), Token.balanceOf(address(this)) ));
         }
         
         // destroct contract send funds to _vault 
